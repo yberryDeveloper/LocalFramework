a9bf66997b7b32e43ffe2ec8f5c993d590a95f36
Neelesh Jain <neeleshjain@server.neeleshjain.private>
2018-09-04 11:23:35 +0530

    Finished Our Fremwork

---
+++ LocalFramework/LocalFramework/LocalFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocalFramework.h
+//  LocalFramework
+//
+//  Created by Neelesh Jain on 9/4/18.
+//  Copyright © 2018 Neelesh Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalFramework.
+FOUNDATION_EXPORT double LocalFrameworkVersionNumber;
+
+//! Project version string for LocalFramework.
+FOUNDATION_EXPORT const unsigned char LocalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFramework/PublicHeader.h>
+
+
---
+++ LocalFramework/LocalFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LocalFramework
+//
+//  Created by Neelesh Jain on 9/4/18.
+//  Copyright © 2018 Neelesh Jain. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init() {}
+    
+    public static func doSomething() -> String{
+        return "Stuff Here YOO!"
+    }
+}
